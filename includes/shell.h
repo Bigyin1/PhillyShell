9176ebc2f15ac38c498b17f7ec958e7744b0b5e6
_ser_ <sergei.aparin1955@yandex.ru>
2021-04-09 16:13:12 +0300

    v0.13 (restructured project)

--- includes/shell.h
+++
@@ -1,6 +0,0 @@
-#ifndef PHILLYSHELL_SHELL_H
-#define PHILLYSHELL_SHELL_H
-
-int start (int argc, char **argv, char **environ);
-
-#endif // PHILLYSHELL_SHELL_H
--- main.c
+++
@@ -1,9 +0,0 @@
-#include "shell.h"
-#include <stdio.h>
-
-int
-main (int argc, char **argv, char **environ)
-{
-
-  return start(argc, argv, environ);
-}
